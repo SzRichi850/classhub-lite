865de7b29ef52a6c120a11ad8137d97577115579
Blaixx <totadam0531@gmail.com>
2026-05-19 14:17:21 +0200

    Button

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 const mediaQuery = window.matchMedia('(max-width: 768px)');
-const fomenu = document.querySelector('.fomenu')
+const fomenu = document.querySelector('.fomenu');
+const button = document.querySelector('.button');
 
 function KepernyoValtozas(e) {
   if (e.matches) {
     console.log("📱 FIXEN MŰKÖDIK: Mobil nézetre váltottunk! (768px alatt)");
     fomenu.style.display = 'none';
+    button.style.display = '';
+    
   } else {
     console.log("💻 FIXEN MŰKÖDIK: Asztali nézetre váltottunk! (768px felett)");
     fomenu.style.display = '';
+    button.style.display = 'none';
   }
 }
 

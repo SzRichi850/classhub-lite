01694f00ac1346233a0824591449cc1a3afbb5e6
budinszkibencemax <budinszkibence-10310@taszi.hu>
2026-05-20 13:48:51 +0200

    Like kiírás, js to html
    ncsd

--- js/posts.js
+++ js/posts.js
@@ -1,5 +1,8 @@
 const likeButtons = document.querySelectorAll(".like-btn");
 
+const like = document.getElementById(like).innerHTML = like_count;
+let like_count = 0;
+
 likeButtons.forEach(button => {
 
     let liked = false;
@@ -10,8 +13,11 @@ likeButtons.forEach(button => {
 
         if (liked) {
             button.textContent = "💖 Liked";
-        } else {
+            like_count += 1; 
+        } 
+        else {
             button.textContent = "❤️ Like";
+            like_count -= 1;
         }
 
     });

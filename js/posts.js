3088553018f35671f03bb46d0097897661af2fba
asd <grohmark20@gmail.com>
2026-05-22 10:18:45 +0200

    Komment sytem
    feat: Komment rendszer meg csinálva

--- js/posts.js
+++ js/posts.js
@@ -17,3 +17,43 @@ likeButtons.forEach(button => {
     });
 
 });
+
+
+// Fake comment sytem
+const commentsContainer = document.getElementById("comments");
+
+    function addComment() {
+      const message = document.getElementById("message").value.trim();
+
+      if(message === ""){
+        alert("Tölts ki minden mezőt!");
+        return;
+      }
+
+      const comment = document.createElement("div");
+      comment.classList.add("comment");
+
+      const now = new Date();
+      const time =
+        now.getHours().toString().padStart(2, "0") +
+        ":" +
+        now.getMinutes().toString().padStart(2, "0");
+
+      comment.innerHTML = `
+        <button class="delete-btn">Törlés</button>
+        <p>${message}</p>
+        <div class="time">${time}</div>
+      `;
+
+      comment.querySelector(".delete-btn").addEventListener("click", () => {
+        comment.remove();
+      });
+
+      commentsContainer.prepend(comment);
+
+      document.getElementById("name").value = "";
+      document.getElementById("message").value = "";
+
+    }
+
+      commentsContainer.appendChild(comment);
